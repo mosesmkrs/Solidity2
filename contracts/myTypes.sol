aa47210969c64002b241db533f966d7cf4ee93e3
mosesmkrs <karisamoses392@gmail.com>
2024-04-02 12:11:36 +0300

    Lesson2

---
+++ contracts/myTypes.sol
@@ -0,0 +1,19 @@
+
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+//I can use nutspec formst !!🥳
+/// @author mosesmkrs
+
+contract MyTypes {
+
+    // NB: storing the data in their own "bubbles" helps optimize your contract.
+    bool public myBool = true;
+
+    uint8 public myUint8 = 255;
+
+    int256 private myInt8 = -565878973739898000389998; 
+
+    address public previousContractAddress = 0xf8e81D47203A594245E36C48e151709F0C19fBe8;
+}
